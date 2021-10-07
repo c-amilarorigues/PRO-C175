9d148b60fdfacc0995b945bc43d107c0f32a6a1e
c-amilarorigues <81708586+c-amilarorigues@users.noreply.github.com>
2021-10-07 20:54:26 -0300

    Update markerhandler.js

--- js/markerhandler.js
+++ js/markerhandler.js
@@ -14,13 +14,13 @@ AFRAME.registerComponent("markerhandler", {
       var barcodeValue = this.el.getAttribute("value");
       elementsArray.push({ element_name: elementName, barcode_value: barcodeValue });
 
-      // Changing Compound Visiblity
+      // Alterando a Visibilidade do Componente
       compounds[barcodeValue]["compounds"].map(item => {
         var compound = document.querySelector(`#${item.compound_name}-${barcodeValue}`);
         compound.setAttribute("visible", false);
       });
 
-      // Changing Atom Visiblity
+      // Alterando a Visibilidade do Atom
       var atom = document.querySelector(`#${elementName}-${barcodeValue}`);
       atom.setAttribute("visible", true);
     });
@@ -87,7 +87,7 @@ AFRAME.registerComponent("markerhandler", {
       }
     }
   },
-  //Calculate distance between two position markers
+  //Calcule a distância entre a posição de dois marcadores
   getDistance: function (elA, elB) {
     return elA.object3D.position.distanceTo(elB.object3D.position);
   },
@@ -115,17 +115,17 @@ AFRAME.registerComponent("markerhandler", {
     }
   },
   showCompound: function (compound) {
-    //Hide elements
+    //Ocultar elementos
     elementsArray.map(item => {
       var el = document.querySelector(`#${item.element_name}-${item.barcode_value}`);
       el.setAttribute("visible", false);
     });
-    //Show Compound
+    //Mostrar Composto
     var compound = document.querySelector(`#${compound.name}-${compound.value}`);
     compound.setAttribute("visible", true);
   },
   getCompounds: function () {
-    // NOTE: Use ngrok server to get json values
+    // OBSERVAÇÃO: Use o servidor ngrok para obter os valores json
     return fetch("js/compoundList.json")
       .then(res => res.json())
       .then(data => data);
